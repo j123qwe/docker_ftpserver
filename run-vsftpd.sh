772b1fb4940b57153832f73c231db1452b594376
Josh Davis <jdavis@ntwls.com>
2020-02-21 09:57:48 -0600

    first commit

---
+++ run-vsftpd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Create home dir and update vsftpd user db:
+mkdir -p /home/vsftpd
+chown -R ftp:ftp /home/vsftpd
+echo -e "${FTP_USER}\n${FTP_PASS}" > /etc/vsftpd/virtual_users.txt
+db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
+
+# Set passive mode parameters:
+if [ "${PASV_ADDRESS}" = "REQUIRED" ]; then
+	echo "Please add the IPv4 address of your host in your docker run command: -e PASV_ADDRESS=\${IP} "
+	exit 1
+fi
+echo "pasv_address=${PASV_ADDRESS}" >> /etc/vsftpd/vsftpd.conf
+
+# Set listen port:
+echo "listen_port=${LISTEN_PORT}" >> /etc/vsftpd/vsftpd.conf
+
+# Set data port:
+echo "ftp_data_port=${DATA_PORT}" >> /etc/vsftpd/vsftpd.conf
+
+# Run vsftpd:
+vsftpd /etc/vsftpd/vsftpd.conf
